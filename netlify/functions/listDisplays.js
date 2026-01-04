606af776a33510b7d954e61938b8f9214863d70a
immelbr87-ux <immelbr87@gmail.com>
2026-01-03 22:55:55 -0600

    Update listDisplays.js

--- netlify/functions/listDisplays.js
+++ netlify/functions/listDisplays.js
@@ -1,1 +1,8 @@
-
+exports.handler = async () => {
+  // TEMP: returns empty list so your frontend can prove it’s hitting JSON
+  return {
+    statusCode: 200,
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ items: [] })
+  };
+};
