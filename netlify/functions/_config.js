5a443c846ed45ec34e8f26ce86b10f82ee787b40
immelbr87-ux <immelbr87@gmail.com>
2026-01-30 16:01:54 -0600

    Update _config.js

--- netlify/functions/_config.js
+++ netlify/functions/_config.js
@@ -1,5 +1,21 @@
+// Centralized environment config
+
+console.log("Loading config...");
+
 module.exports = {
-  stripeSecret: process.env.STRIPE_SECRET_KEY,
-  resendApiKey: process.env.RESEND_API_KEY,
-  baseUrl: process.env.URL // Netlify provides this
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+
+  adminSecret: process.env.ADMIN_SECRET_TOKEN,
+
+  siteUrl: process.env.SITE_URL,
+
+  mailerSendApiKey: process.env.MAILERSEND_API_KEY,
+  fromEmail: process.env.MAILERSEND_FROM_EMAIL,
+  fromName: process.env.MAILERSEND_FROM_NAME,
+
+  airtableApiKey: process.env.AIRTABLE_API_KEY,
+  airtableBaseId: process.env.AIRTABLE_BASE_ID,
+  airtableTable: process.env.AIRTABLE_TABLE,
+  airtableView: process.env.AIRTABLE_VIEW,
 };
