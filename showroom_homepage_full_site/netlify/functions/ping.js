07aa5e71a0e6978b6e1ebde4cc22db9152acb4dc
immelbr87-ux <immelbr87@gmail.com>
2026-02-01 16:24:42 -0600

    Delete showroom_homepage_full_site directory

--- showroom_homepage_full_site/netlify/functions/ping.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ ok: true, ts: Date.now() })
-  };
-};
